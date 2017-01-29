de107099bef4da72bba4d72a121d24a55ef1ab83
Tomohiro <itomomoti@gmail.com>
2017-01-29 14:10:52 +0900

    not to pass 0 to malloc, realloc

--- MemUtil.hpp
+++ MemUtil.hpp
@@ -2,12 +2,15 @@
 #define INCLUDE_GUARD_MemUtil
 
 #include <stdint.h> // include uint64_t etc.
+#include <assert.h>
 #include <iostream>
 
 namespace memutil
 {
   template <typename T>
   T * mymalloc(size_t n) {
+    assert(n > 0);
+
     T * ptr = static_cast<T *>(malloc(n * sizeof(T)));
     if (ptr == NULL) {
       std::cout << "not enough memory at line " << __LINE__ << std::endl;
@@ -16,8 +19,11 @@ namespace memutil
     return ptr;
   }
 
+
   template <typename T>
   void myrealloc(T *& ptr, size_t n) {
+    assert(n > 0);
+
     ptr = static_cast<T *>(realloc(ptr, n * sizeof(T)));
     if (ptr == NULL) {
       std::cout << "not enough memory at line " << __LINE__ << std::endl;
@@ -25,11 +31,13 @@ namespace memutil
     }
   }
 
+
   template <typename T>
   void myfree(T *& ptr) {
     free(ptr);
     ptr = NULL;
   }
+
 
   template <typename T>
   void mydelete(T *& ptr) {
