c54c84b4744bfcf4a2855f4ce07dfdef438dd226
Tomohiro <itomomoti@gmail.com>
2017-03-07 13:17:38 +0900

    added Uncopyable.hpp

---
+++ Uncopyable.hpp
@@ -0,0 +1,8 @@
+class Uncopyable
+{
+ protected:
+  Uncopyable() = default;
+  ~Uncopyable() = default;
+  Uncopyable(const Uncopyable&) = delete;
+  Uncopyable& operator=(const Uncopyable&) = delete;
+};
