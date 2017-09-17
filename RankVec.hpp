f70e9a73807d06dd4d9fb85d1edfd64a9c7bf7e3
Tomohiro I <itomomoti@gmail.com>
2017-09-17 22:32:04 +0900

    implement copy, move. implement shorten. bugfix on reservation of blockM_

--- RankVec.hpp
+++ RankVec.hpp
@@ -33,18 +33,140 @@
 template<uint16_t BSIZE_M = 256, uint16_t BSIZE_T = 4096>
 class RankVec
 {
-  BitVec bv;
+  BitVec bv_;
   uint16_t * blockM_;
   uint64_t * blockT_;
 
 public:
+  /*!
+   * @brief Constructor.
+   */
   RankVec
   (
    size_t capacity = 0 //!< Initial capacity.
-   ) : bv(), blockM_(nullptr), blockT_(nullptr) {
+   ) : bv_(), blockM_(nullptr), blockT_(nullptr) {
     assert(capacity <= ctcbits::UINTW_MAX(58));
 
-    reserve(capacity);
+    this->changeCapacity(capacity);
+  }
+
+
+  /*!
+   * @brief Deconstructor.
+   */
+  ~RankVec()
+  {
+    free(blockM_);
+    free(blockT_);
+    // Deconstructor of bv_ is called.
+  }
+
+
+  //// Copy
+  /*!
+   * @brief Copy constructor.
+   * @attention
+   *   Since the contents of 'other' are copied, it may take time when other.size_ is large.
+   */
+  RankVec
+  (
+   const RankVec & other
+   ) : bv_(other.bv_), blockM_(nullptr), blockT_(nullptr) {
+    const auto size = other.size();
+    if (size > 0) {
+      {
+        const auto capacity = this->capacity();
+        const auto lenT = (capacity + BSIZE_T - 1) / BSIZE_T;
+        const auto lenM = (capacity + BSIZE_M - 1) / BSIZE_M - capacity / BSIZE_T;
+        memutil::realloc_AbortOnFail<uint16_t>(blockM_, lenM);
+        memutil::realloc_AbortOnFail<uint64_t>(blockT_, lenT);
+      }
+      {
+        const auto lenT = (size + BSIZE_T - 1) / BSIZE_T;
+        const auto lenM = (size + BSIZE_M - 1) / BSIZE_M - size / BSIZE_T;
+        for (uint64_t i = 0; i < lenT; ++i) {
+          blockT_[i] = other.blockT_[i];
+        }
+        for (uint64_t i = 0; i < lenM; ++i) {
+          blockM_[i] = other.blockM_[i];
+        }
+      }
+    }
+  }
+
+
+  /*!
+   * @brief Assignment operator.
+   * @attention
+   *   - It may take time when other.size_ is large.
+   *   - The original contents of "this" are freed.
+   */
+  RankVec & operator=
+  (
+   const RankVec & other
+   ) {
+    if (this != &other) {
+      this->clear(); this->changeCapacity(); // clear() & changeCapacity() free array_
+      bv_ = other.bv_;
+      {
+        const auto capacity = other.capacity();
+        const auto lenT = (capacity + BSIZE_T - 1) / BSIZE_T;
+        const auto lenM = (capacity + BSIZE_M - 1) / BSIZE_M - capacity / BSIZE_T;
+        memutil::realloc_AbortOnFail<uint16_t>(blockM_, lenM);
+        memutil::realloc_AbortOnFail<uint64_t>(blockT_, lenT);
+      }
+      {
+        const auto size = other.size();
+        const auto lenT = (size + BSIZE_T - 1) / BSIZE_T;
+        const auto lenM = (size + BSIZE_M - 1) / BSIZE_M - size / BSIZE_T;
+        for (uint64_t i = 0; i < lenT; ++i) {
+          blockT_[i] = other.blockT_[i];
+        }
+        for (uint64_t i = 0; i < lenM; ++i) {
+          blockM_[i] = other.blockM_[i];
+        }
+      }
+    }
+    return *this;
+  }
+
+
+  //// Move
+  /*!
+   * @brief Move constructor.
+   * @attention
+   *   'other' is initialized to an object with capacity = 0.
+   */
+  RankVec
+  (
+   RankVec && other
+   ) : bv_(std::move(other.bv_)) {
+    blockM_ = other.blockM_;
+    blockT_ = other.blockT_;
+    other.blockM_ = nullptr;
+    other.blockT_ = nullptr;
+  }
+
+
+  /*!
+   * @brief Move operator.
+   * @attention
+   *   - The original contents of "this" are freed.
+   *   - "other" is initialized to an object with capacity = 0.
+   */
+  RankVec & operator=
+  (
+   RankVec && other
+   ) {
+    if (this != &other) {
+      this->clear(); this->changeCapacity(); // clear() & changeCapacity() free array_
+      this->bv_ = std::move(other.bv_);
+      blockM_ = other.blockM_;
+      blockT_ = other.blockT_;
+      other.blockM_ = nullptr;
+      other.blockT_ = nullptr;
+    }
+    return *this;
   }
 
 
@@ -53,7 +175,7 @@ public:
    */
   const uint64_t * getConstArrayPtr() const noexcept
   {
-    return bv.getConstArrayPtr();
+    return bv_.getConstArrayPtr();
   }
 
 
@@ -70,7 +192,7 @@ public:
    ) const noexcept {
     assert(w <= 64);
 
-    return bv.readWBits(bitPos, w, mask);
+    return bv_.readWBits(bitPos, w, mask);
   }
 
 
@@ -84,7 +206,7 @@ public:
    ) const noexcept {
     assert(bits::bitSize(mask) + (bitPos & 0x3f) <= 64);
 
-    return bv.readWBits_S(bitPos, mask);
+    return bv_.readWBits_S(bitPos, mask);
   }
 
 
@@ -95,9 +217,9 @@ public:
   (
    const uint64_t bitPos //!< in [0, capacity).
    ) const noexcept {
-    assert(bitPos < bv.capacity());
+    assert(bitPos < bv_.capacity());
 
-    return bv.readWBits_S(bitPos);
+    return bv_.readWBits_S(bitPos, ctcbits::UINTW_MAX(1));
   }
 
 
@@ -109,11 +231,11 @@ public:
   (
    const bool val //!< Bool value.
    ) {
-    assert(bv.size() < bv.capacity());
+    assert(bv_.size() < bv_.capacity());
 
-    const auto pos = bv.size();
-    bv.resize(pos + 1);
-    bv.writeBit(val, pos);
+    const auto pos = bv_.size();
+    bv_.resize(pos + 1);
+    bv_.writeBit(val, pos);
 
     if (pos == 0) {
       blockT_[0] = blockM_[0] = val;
@@ -139,13 +261,79 @@ public:
 
 
   /*!
-   * @brief Rank query.
+   * @brief Shorten bit array to given size while updating rank/select DS.
+   * @note It does nothing if given size is larger than or equal to current size.
    */
-  uint64_t rank
+  void shorten
+  (
+   const size_t givenSize //!< Given size.
+   ) {
+    if (givenSize >= bv_.size()) {
+      return;
+    }
+    bv_.resize(givenSize);
+    if (givenSize == 0) {
+      return;
+    }
+
+    const auto pos = givenSize - 1; // New last pos.
+    const auto idxT = pos / BSIZE_T;
+    const auto remT = pos % BSIZE_T;
+    const auto idxM = (pos / BSIZE_M) - idxT;
+    const auto onesLastBlockM = bits::cnt_1(bv_.getConstArrayPtr() + pos / BSIZE_M * BSIZE_M / 64, pos % BSIZE_M);
+    const auto onesLastBlockT = (remT >= BSIZE_M)? blockM_[idxM-1] + onesLastBlockM : onesLastBlockM;
+    if (remT < BSIZE_T - BSIZE_M) {
+      blockM_[idxM] = onesLastBlockT;
+    }
+    blockT_[idxT] = (idxT)? blockT_[idxT - 1] + onesLastBlockT : onesLastBlockT;
+  }
+
+  
+  /*!
+   * @brief Get # 0 bits in the bit vec.
+   */
+  uint64_t getNum_0() const noexcept {
+    return bv_.size() - getNum_1();
+  }
+
+
+  /*!
+   * @brief Get # 1 bits in the bit vec.
+   */
+  uint64_t getNum_1() const noexcept {
+    return (bv_.size())? blockT_[(bv_.size() - 1) / BSIZE_T] : 0;
+  }
+
+
+  /*!
+   * @brief Rank query for unset bits.
+   */
+  uint64_t rank_0
   (
    const uint64_t pos //!< pos in [0, size).
    ) const noexcept {
-    assert(pos < bv.size());
+    assert(pos < bv_.size());
+
+    const auto size = bv_.size();
+    const auto idxT = pos / BSIZE_T;
+    const auto remT = pos % BSIZE_T;
+    const auto idxM = (pos / BSIZE_M) - idxT;
+    uint64_t rank = (idxT)? (idxT * BSIZE_T) - blockT_[idxT - 1] : 0;
+    if (remT >= BSIZE_M) {
+      rank += (remT / BSIZE_M * BSIZE_M) - blockM_[idxM - 1];
+    }
+    return rank + bits::cnt_0(bv_.getConstArrayPtr() + (pos / BSIZE_M * BSIZE_M / 64), pos % BSIZE_M);
+  }
+
+
+  /*!
+   * @brief Rank query for set bits.
+   */
+  uint64_t rank_1
+  (
+   const uint64_t pos //!< pos in [0, size).
+   ) const noexcept {
+    assert(pos < bv_.size());
 
     const auto idxT = pos / BSIZE_T;
     const auto remT = pos % BSIZE_T;
@@ -154,23 +342,76 @@ public:
     if (remT >= BSIZE_M) {
       rank += blockM_[idxM - 1];
     }
-    return rank + bits::cnt(bv.getConstArrayPtr() + (pos / BSIZE_M * BSIZE_M / 64), pos % BSIZE_M);
+    return rank + bits::cnt_1(bv_.getConstArrayPtr() + (pos / BSIZE_M * BSIZE_M / 64), pos % BSIZE_M);
   }
 
 
   /*!
-   * @brief Select query.
+   * @brief Select query for unset bits.
    * @pre The answer must be found.
    * @attention O(log size) time.
    */
-  uint64_t select
+  uint64_t select_0
   (
-   uint64_t rank //!< 1base rank [1, rank(bv.size()-1)].
+   uint64_t rank //!< 1base rank [1, #0's].
    ) const noexcept {
     assert(rank > 0);
-    assert(rank <= blockT_[(bv.size() - 1) / BSIZE_T]);
+    assert(rank <= getNum_0());
 
-    const auto idxT = basic_search::lb(blockT_, rank, 0, ((bv.size() - 1) / BSIZE_T) + 1);
+    const auto idxT = basic_search::partition_idx
+      (
+        0, ((bv_.size() - 1) / BSIZE_T) + 1,
+        [&](uint64_t i) { return (i + 1) * BSIZE_T - blockT_[i] >= rank; }
+       );
+    const auto posT = idxT * BSIZE_T;
+    rank -= (idxT > 0)? BSIZE_T * idxT - blockT_[idxT - 1] : 0;
+    const auto idxM = (posT / BSIZE_M) - idxT;
+    uint64_t i = 0;
+    while (i < (BSIZE_T / BSIZE_M) - 1 && BSIZE_M * (i+1) - blockM_[idxM + i] < rank) {
+      ++i;
+    }
+    rank -= (i > 0)? BSIZE_M * i - blockM_[idxM + i - 1] : 0;
+    const auto posM = posT + i * BSIZE_M;
+    return posM + bits::sel_0(bv_.getConstArrayPtr() + (posM / 64), rank);
+  }
+
+
+  /*!
+   * @brief Select query for set bits.
+   * @pre The answer must be found.
+   * @attention O(log size) time.
+   */
+  uint64_t select_1
+  (
+   uint64_t rank //!< 1base rank [1, #1's].
+   ) const noexcept {
+    assert(rank > 0);
+    assert(rank <= getNum_1());
+
+    if (rank == 3999) {
+      const auto idxT = basic_search::partition_idx
+        (
+         0, ((bv_.size() - 1) / BSIZE_T) + 1,
+         [&](uint64_t i) { return blockT_[i] >= rank; }
+         );
+      const auto posT = idxT * BSIZE_T;
+      rank -= (idxT > 0)? blockT_[idxT - 1] : 0;
+      const auto idxM = (posT / BSIZE_M) - idxT;
+      uint64_t i = 0;
+      while (i < (BSIZE_T / BSIZE_M) - 1 && blockM_[idxM + i] < rank) {
+        ++i;
+      }
+      rank -= (i > 0)? blockM_[idxM + i - 1] : 0;
+      const auto posM = posT + i * BSIZE_M;
+      return posM + bits::sel_1(bv_.getConstArrayPtr() + (posM / 64), rank);
+    }
+
+
+    const auto idxT = basic_search::partition_idx
+      (
+       0, ((bv_.size() - 1) / BSIZE_T) + 1,
+       [&](uint64_t i) { return blockT_[i] >= rank; }
+       );
     const auto posT = idxT * BSIZE_T;
     rank -= (idxT > 0)? blockT_[idxT - 1] : 0;
     const auto idxM = (posT / BSIZE_M) - idxT;
@@ -180,33 +421,32 @@ public:
     }
     rank -= (i > 0)? blockM_[idxM + i - 1] : 0;
     const auto posM = posT + i * BSIZE_M;
-    return posM + bits::sel(bv.getConstArrayPtr() + (posM / 64), rank);
+    return posM + bits::sel_1(bv_.getConstArrayPtr() + (posM / 64), rank);
   }
 
 
   /*!
-   * @brief Predecessor query.
-   * @note Here bit vector is considered to represent a set of uint by the set of positions of set bits.
-   * @return The largest set bit position smaller than or equal to val. Return UINT64_MAX when not found.
+   * @brief Predecessor query for unset bits.
+   * @return The largest unset bit position smaller than or equal to 'val'. Return UINT64_MAX when not found.
    */
-  uint64_t pred
+  uint64_t pred_0
   (
    uint64_t val //!< val (>= 0).
    ) const noexcept {
-    const auto size = bv.size();
+    const auto size = bv_.size();
     if (size == 0) {
       return UINT64_MAX;
     } else if (val >= size) {
       val = size - 1;
     }
 
-    const auto ans = bits::pred(getConstArrayPtr(), val, std::min(UINT64_C(2), 1 + val / 64));
+    const auto ans = bits::pred_0(getConstArrayPtr(), val, std::min(UINT64_C(2), 1 + val / 64));
     if (ans != UINT64_MAX) {
       return ans;
     }
-    const auto r = rank(val);
+    const auto r = rank_0(val);
     if (r > 0) {
-      return select(r);
+      return select_0(r);
     } else {
       return UINT64_MAX;
     }
@@ -214,26 +454,79 @@ public:
 
 
   /*!
-   * @brief Successor query.
-   * @note Here bit vector is considered to represent a set of uint by the set of positions of set bits.
-   * @return The smallest set bit position larger than or equal to val. Return UINT64_MAX when not found.
+   * @brief Predecessor query for set bits.
+   * @return The largest set bit position smaller than or equal to 'val'. Return UINT64_MAX when not found.
    */
-  uint64_t succ
+  uint64_t pred_1
   (
    uint64_t val //!< val (>= 0).
    ) const noexcept {
-    const auto size = bv.size();
+    const auto size = bv_.size();
+    if (size == 0) {
+      return UINT64_MAX;
+    } else if (val >= size) {
+      val = size - 1;
+    }
+
+    const auto ans = bits::pred_1(getConstArrayPtr(), val, std::min(UINT64_C(2), 1 + val / 64));
+    if (ans != UINT64_MAX) {
+      return ans;
+    }
+    const auto r = rank_1(val);
+    if (r > 0) {
+      return select_1(r);
+    } else {
+      return UINT64_MAX;
+    }
+  }
+
+
+  /*!
+   * @brief Successor query for unset bits.
+   * @return The smallest unset bit position larger than or equal to val. Return UINT64_MAX when not found.
+   */
+  uint64_t succ_0
+  (
+   uint64_t val //!< val (>= 0).
+   ) const noexcept {
+    const auto size = bv_.size();
     if (val >= size) {
       return UINT64_MAX;
     }
 
-    const auto ans = bits::succ(getConstArrayPtr(), val, std::min(UINT64_C(2), 1 + (size - val - 1) / 64));
+    const auto ans = bits::succ_0(getConstArrayPtr(), val, std::min(UINT64_C(2), 1 + (size - 1)/64 - val/64));
     if (ans < size) {
       return ans;
     }
-    const auto r = rank(val);
-    if (r < blockT_[(bv.size() - 1) / BSIZE_T]) {
-      return select(r+1);
+    const auto r = rank_0(val);
+    if (r < getNum_0()) {
+      return select_0(r+1);
+    } else {
+      return UINT64_MAX;
+    }
+  }
+
+
+  /*!
+   * @brief Successor query for set bits.
+   * @return The smallest set bit position larger than or equal to val. Return UINT64_MAX when not found.
+   */
+  uint64_t succ_1
+  (
+   uint64_t val //!< val (>= 0).
+   ) const noexcept {
+    const auto size = bv_.size();
+    if (val >= size) {
+      return UINT64_MAX;
+    }
+
+    const auto ans = bits::succ_1(getConstArrayPtr(), val, std::min(UINT64_C(2), 1 + (size - 1)/64 - val/64));
+    if (ans < size) {
+      return ans;
+    }
+    const auto r = rank_1(val);
+    if (r < getNum_1()) {
+      return select_1(r+1);
     } else {
       return UINT64_MAX;
     }
@@ -244,7 +537,7 @@ public:
    * @brief Get current capacity.
    */
   size_t capacity() const noexcept {
-    return bv.capacity();
+    return bv_.capacity();
   }
 
 
@@ -252,7 +545,7 @@ public:
    * @brief Get current size.
    */
   size_t size() const noexcept {
-    return bv.size();
+    return bv_.size();
   }
 
 
@@ -260,13 +553,13 @@ public:
    * @brief Calculate total memory usage in bytes.
    */
   size_t calcMemBytes() const noexcept {
-    const auto capacity = bv.capacity();
+    const auto capacity = bv_.capacity();
     const auto lenT = (capacity + BSIZE_T - 1) / BSIZE_T;
-    const auto lenM = (capacity + BSIZE_M - 1) / BSIZE_M - lenT;
-    size_t bytes = bv.calcMemBytes();
+    const auto lenM = (capacity + BSIZE_M - 1) / BSIZE_M - capacity / BSIZE_T;
+    size_t bytes = sizeof(*this);
     bytes += sizeof(blockM_[0]) * lenM;
     bytes += sizeof(blockT_[0]) * lenT;
-    bytes += sizeof(*this);
+    bytes += bv_.calcMemBytes();
     return bytes;
   }
 
@@ -275,7 +568,7 @@ public:
    * @brief Return if no element is stored.
    */
   bool empty() const noexcept {
-    return bv.empty();
+    return bv_.empty();
   }
 
 
@@ -283,63 +576,57 @@ public:
    * @brief Clear vector. It only changes size to zero.
    */
   void clear() noexcept {
-    bv.resize(0);
+    bv_.resize(0);
   }
 
 
   /*!
-   * @brief Reserve.
-   * @note
-   *   This function does not shrink 'capacity_'
-   *   If 'newCapacity > oldCapacity', realloc arrays. 'size' is unchanged.
-  */
-  void reserve
-  (
-   const size_t newCapacity
-   ) {
-    assert(newCapacity <= ctcbits::UINTW_MAX(58));
-
-    const auto oldCapacity = bv.capacity();
-    if (newCapacity > oldCapacity) {
-      bv.reserve(newCapacity);
-      const auto lenT = (newCapacity + BSIZE_T - 1) / BSIZE_T;
-      const auto lenM = (newCapacity + BSIZE_M - 1) / BSIZE_M - lenT;
-      memutil::realloc_AbortOnFail<uint16_t>(blockM_, lenM);
-      memutil::realloc_AbortOnFail<uint64_t>(blockT_, lenT);
-    }
-  }
-
-
-  /*!
-   * @brief Shrink vector to fit current bit-length in use.
+   * @brief Change capacity to max of givenCapacity and current size.
+   * @node If givenCapacity is not given, it works (with default parameter 0) as shrink_to_fit.
    */
-  void shrink_to_fit() {
-    const auto size = bv.size();
-    if (size > 0) {
-      bv.shrink_to_fit();
-      const auto lenT = (size + BSIZE_T - 1) / BSIZE_T;
-      const auto lenM = (size + BSIZE_M - 1) / BSIZE_M - lenT;
-      memutil::realloc_AbortOnFail<uint16_t>(blockM_, lenM);
-      memutil::realloc_AbortOnFail<uint64_t>(blockT_, lenT);
-    } else {
-      memutil::safefree(blockM_);
-      memutil::safefree(blockT_);
+  void changeCapacity
+  (
+   const size_t givenCapacity = 0
+   ) {
+    if (bv_.capacity() != givenCapacity) {
+      bv_.changeCapacity(givenCapacity);
+      const auto newCapacity = bv_.capacity();
+      if (newCapacity > 0) {
+        const auto lenT = (newCapacity + BSIZE_T - 1) / BSIZE_T;
+        const auto lenM = (newCapacity + BSIZE_M - 1) / BSIZE_M - newCapacity / BSIZE_T;
+        memutil::realloc_AbortOnFail<uint16_t>(blockM_, lenM);
+        memutil::realloc_AbortOnFail<uint64_t>(blockT_, lenT);
+      } else {
+        memutil::safefree(blockM_);
+        memutil::safefree(blockT_);
+      }
     }
   }
 
 
-  void printDebugInfo() const {
-    const auto lenT = (bv.size() + BSIZE_T - 1) / BSIZE_T;
-    const auto lenM = (bv.size() + BSIZE_M - 1) / BSIZE_M - lenT;
-    for (uint64_t i = 0; i < lenT; ++i) {
-      std::cout << blockT_[i] << ", ";
+  void printStatistics
+  (
+   const bool verbose = false
+   ) const noexcept {
+    std::cout << "RankVec object (" << this << ") " << __func__ << "(" << verbose << ") BEGIN" << std::endl;
+    std::cout << "size = " << bv_.size() << ", capacity = " << bv_.capacity() << ", BSIZE_T = " << BSIZE_T << ", BSIZE_M = " << BSIZE_M << std::endl;
+    std::cout << this->calcMemBytes() << " bytes" << std::endl;
+    if (verbose) {
+      const auto lenT = (bv_.size() + BSIZE_T - 1) / BSIZE_T;
+      const auto lenM = (bv_.size() + BSIZE_M - 1) / BSIZE_M - bv_.size() / BSIZE_T;
+      std::cout << "dump blockT_[0.." << lenT << ")" << std::endl;
+      for (uint64_t i = 0; i < lenT; ++i) {
+        std::cout << blockT_[i] << ", ";
+      }
+      std::cout << std::endl;
+      std::cout << "dump blockM_[0.." << lenM << ")" << std::endl;;
+      for (uint64_t i = 0; i < lenM; ++i) {
+        std::cout << blockM_[i] << ", ";
+      }
+      std::cout << std::endl;
+      bv_.printStatistics(verbose);
     }
-    std::cout << std::endl;
-    for (uint64_t i = 0; i < lenM; ++i) {
-      std::cout << blockM_[i] << ", ";
-    }
-    std::cout << std::endl;
-    bv.printDebugInfo();
+    std::cout << "RankVec object (" << this << ") " << __func__ << "(" << verbose << ") END" << std::endl;
   }
 };
 
